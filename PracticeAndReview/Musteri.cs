f2a067ee4d26c683a588482f23dac34650d96967
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-08-30 18:29:07 +0300

    Add project files.

---
+++ PracticeAndReview/Musteri.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndReview
+{
+    internal class Musteri
+    {
+        public int id;
+        private string ad;
+        public string soyad;
+        public int yas;
+        public string meslek;
+        public string sehir;
+        
+        
+
+        public string Ad { get => ad; set => ad = value.ToLower(); }
+        public string Soyad { get => soyad; set => soyad = value.ToUpper(); }
+
+        public string Meslek { get => meslek; set => meslek = value.ToLower(); }
+        public string Sehir { get => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(sehir);
+            set => sehir = value; }
+    }
+}
---
+++ PracticeAndReview/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAndReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            Console.WriteLine("id girin : ");
+            musteri.id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adiniz : ");
+            musteri.Ad = Console.ReadLine();
+            Console.WriteLine("SoyAdiniz : ");
+            musteri.Soyad = Console.ReadLine();
+            Console.WriteLine("Yasiniz : ");
+            musteri.yas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mesleginiz : ");
+            musteri.Meslek = Console.ReadLine();
+            Console.WriteLine("Sehriniz : ");
+            musteri.Sehir = Console.ReadLine();
+
+            Console.WriteLine("***** Musteri Kimlik Karti *****");
+            Console.WriteLine();
+            Console.WriteLine(musteri.id + "-" + musteri.Ad + " " + musteri.Soyad + " " + musteri.yas + " " + musteri.meslek + " " + musteri.Sehir);
+
+            Console.Read();
+        }
+    }
+}
